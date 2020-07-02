525b40c74dddae3ed3d14181e3d188abe64200ba
Xav83 <x.jouvenot@gmail.com>
2020-07-02 09:33:12 +0200

    Extract some cmake functions from the cmake code.

--- .cmake/cppcheck.cmake
+++ .cmake/cppcheck.cmake
@@ -5,10 +5,28 @@ set(RUN_CPPCHECK
       "Sets to ON if you want to run cppcheck during your project compilation. OFF by default"
 )
 
-if(NOT RUN_CPPCHECK)
-  message(STATUS "Cppcheck deactivated during compilation.")
-else()
-  message(STATUS "Cppcheck activated during compilation.")
+function(get_cppcheck_program output)
+  find_program(
+    CPPCHECK_EXE
+    NAMES cppcheck cppcheck.exe
+    HINTS
+    "C:\\Program Files\\Cppcheck" # chocolatey default path
+    DOC "Cppcheck program"
+    REQUIRED
+    )
+
+  if(NOT CPPCHECK_EXE)
+    message(
+      FATAL_ERROR
+        "Cppcheck not found.
+        You can install it using Homebrew on Apple or Chocolatey on Windows.")
+    return()
+  endif()
+
+  set(${output} ${CPPCHECK_EXE} PARENT_SCOPE)
+endfunction()
+
+function(cppcheck_version output)
   find_program(
     CMAKE_CXX_CPPCHECK
     NAMES cppcheck cppcheck.exe
@@ -36,6 +54,19 @@ else()
   endif()
 
   string(REPLACE "Cppcheck " "" CPPCHECK_VERSION_OUTPUT ${CPPCHECK_VERSION_OUTPUT})
+  string(REPLACE "\n" "" CPPCHECK_VERSION_OUTPUT ${CPPCHECK_VERSION_OUTPUT})
+
+  set(${output} ${CPPCHECK_VERSION_OUTPUT} PARENT_SCOPE)
+endfunction()
+
+if(NOT RUN_CPPCHECK)
+  message(STATUS "Cppcheck deactivated during compilation.")
+else()
+  message(STATUS "Cppcheck activated during compilation.")
+
+  get_cppcheck_program(CMAKE_CXX_CPPCHECK)
+
+  cppcheck_version(CPPCHECK_VERSION_OUTPUT)
 
   if(${CPPCHECK_VERSION_OUTPUT} VERSION_GREATER_EQUAL "1.88")
     set(CPPCHECK_STD_FLAG "--std=c++17")
